5fe36b8224c52895ea987e596e63c8aed54e65bf
r4vsdev <r4vsdev@gmail.com>
2022-09-30 00:43:00 -0300

    Add the write and clear screen functions

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let displayVar = '';
+let screen = document.querySelector('.screen')
+
+
 const add = function(a, b) {
 	return a+b;
 };
@@ -12,19 +16,37 @@ const divide = function(a, b) {
     return a / b
 }
 const power = function(x, n) {
-  return Math.pow(x, n);
+	return Math.pow(x, n);
 };
 const sum = function(array) {
     return array.reduce( (total, current) => total + current, 0 );
-  };
-const factorial = function(n) {
-  if (n === 0) return 1;
-  return n * factorial( n - 1 );
 };
-function operate (operator, a, b) {
+const factorial = function(n) {
+	if (n === 0) return 1;
+	return n * factorial( n - 1 );
+};
+function operate (string) {
     if (operator === '+') return add(a, b)
     if (operator === '-') return subtract(a, b)
     if (operator === '*') return multiply(a, b)
     if (operator === '/') return divide(a, b)
     if (operator === '^') return power(a, b)
 }
+function writeOnScreen () {
+    const pressedButton = this.textContent;
+	screen.textContent += pressedButton;
+}
+function clearDisplay () {
+	// displayVar = '';
+	screen.textContent = '';
+	// console.log('display cleared', displayVar)
+}
+
+const btn = document.querySelectorAll('.btn');
+btn.forEach((button) => button.addEventListener('click', writeOnScreen))
+
+const ac = document.querySelector('#ac')
+ac.addEventListener('click', clearDisplay)
+
+const equal = document.querySelector('#equal')
+// equal.addEventListener('click', operate(displayVar))

ff76aef4f3dd0da427615cc399aa3f3c62b365fb
r4vsdev <r4vsdev@gmail.com>
2022-10-05 23:54:28 -0300

    Polish the calculation method

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let firstNumber;
-let secondNumber;
+let numbers = []
 let operator;
 
 const add = function(a, b) {
@@ -26,58 +25,75 @@ const factorial = function(n) {
 	if (n === 0) return 1;
 	return n * factorial( n - 1 );
 };
-function writeOnScreen () {
+function writeNumbers () {
     if (this.id === 'del') return
+    if (this.id === 'equal') return
     const screen = document.querySelector('.screen')
 	screen.textContent += this.textContent;
 }
+function writeOperator () {
+    const screen = document.querySelector('.screen')
+	screen.textContent += this.textContent;
+    operator = this.textContent
+}
 function clearDisplay () {
     const screen = document.querySelector('.screen')
-	screen.textContent = '';
-}
-function operate () {
-    const screen = document.querySelector('.screen')
-    if (operator === '+') screen.textContent += add(firstNumber, secondNumber)
-    if (operator === '-') screen.textContent += subtract(firstNumber, secondNumber)
-    if (operator === 'x') screen.textContent += multiply(firstNumber, secondNumber)
-    if (operator === '/') screen.textContent += divide(firstNumber, secondNumber)
-    if (operator === '^') screen.textContent += power(firstNumber, secondNumber)
-}
-function setFirstNumber () {
-    const screenText = document.querySelector('.screen ').textContent
-    firstNumber      = +screenText
-    operator         = this.textContent
-}
-function setSecondNumber () {
-    const screenText    = document.querySelector('.screen').textContent
-    const index         = screenText.indexOf(operator)
-    secondNumber        = +screenText.slice(index + 1, screenText.length - 1)
+	screen.textContent = ''
+    numbers = []
 }
 function deleteLastChar () {
     const screen = document.querySelector('.screen')
     screen.textContent = screen.textContent.slice(0, -1)
 }
+function setFirstNumber () {
+    const screenText = document.querySelector('.screen ').textContent
+    numbers.push(+screenText)
+    operator = this.textContent
+}
+function setNextNumber () {
+    const screen = document.querySelector('.screen ')
+    console.log(screen.textContent)
+    numbers.push(           // push only the 2nd number to array
+        +screen.textContent
+        .replace(
+            `${numbers[0]}${operator}`
+            , ''))
+    console.log(numbers)
+    numbers[0] = operate(numbers[0], numbers[1])
+    numbers.pop()
+    screen.textContent = numbers[0]
+}
+function operate (firstNumber, secondNumber) {
+    const screen = document.querySelector('.screen')
+    if (operator === '+') return add(firstNumber, secondNumber)
+    if (operator === '-') screen.textContent = subtract(firstNumber, secondNumber)
+    if (operator === 'x') screen.textContent = multiply(firstNumber, secondNumber)
+    if (operator === '/') screen.textContent = divide(firstNumber, secondNumber)
+    if (operator === '^') screen.textContent = power(firstNumber, secondNumber)
+}
 
-// main
-
-// set first number
 const operators = document.querySelectorAll('.operator')
-operators.forEach(button => button.addEventListener('click', setFirstNumber))
+operators.forEach((operator) => operator.addEventListener('click', () => {
+    if (!numbers[0]) {
+        setFirstNumber()
+        return
+    }
+    setNextNumber()
+}))
 
-// write numbers/operators on screen
-const btn = document.querySelectorAll('.btn');
-btn.forEach(button => button.addEventListener('click', writeOnScreen))
+const operatorPad = document.querySelectorAll('.operator');
+operatorPad.forEach((operator) => operator.addEventListener('click', writeOperator))
 
-// clear display
+const numPad = document.querySelectorAll('.number');
+numPad.forEach((number) => number.addEventListener('click', writeNumbers))
+
 const ac = document.querySelector('#ac')
 ac.addEventListener('click', clearDisplay)
 
-const equal = document.querySelector('#equal')
-equal.addEventListener('click', () => {
-    setSecondNumber()
-    operate()
-})
-
-// del
 const del = document.querySelector('#del')
 del.addEventListener('click', deleteLastChar)
+
+const equal = document.querySelector('#equal')
+equal.addEventListener('click', () => {
+    setNextNumber()
+})
